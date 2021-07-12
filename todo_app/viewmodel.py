6189b18944c266d036e2c8c37dcd6f3958f06233
JackieMaw <jackiemaw@gmail.com>
2021-07-12 19:58:13 +0100

    added ViewModel properties: ToDo, Doing, Done

--- todo_app/viewmodel.py
+++ todo_app/viewmodel.py
@@ -1,3 +1,6 @@
+from todo_app.data.task import TaskStatus
+
+
 class ViewModel:
 
     def __init__(self, tasks, repository_description):
@@ -14,4 +17,16 @@ class ViewModel:
 
     @property
     def task_count(self):
-        return len(self._tasks)
+        return len(self._tasks) 
+
+    @property
+    def ToDo(self):
+        return [x for x in self._tasks if x.status == TaskStatus.ToDo]
+
+    @property
+    def Doing(self):
+        return [x for x in self._tasks if x.status == TaskStatus.Doing]
+
+    @property
+    def Done(self):
+        return [x for x in self._tasks if x.status == TaskStatus.Done]
