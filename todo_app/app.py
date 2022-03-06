951084b46708e918b3c5b47d3c7d2817904f428b
JackieMaw <jackiemaw@gmail.com>
2022-03-06 20:04:44 +0000

    Trying to fix gunicorn logging

--- todo_app/app.py
+++ todo_app/app.py
@@ -19,6 +19,11 @@ def create_app():
 
     app = Flask(__name__)
     logging.basicConfig(filename='todo_app\\app.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
     app.config.from_object('todo_app.flask_config.Config')
     login_disabled = os.getenv('LOGIN_DISABLED') == 'True'
     app.config['LOGIN_DISABLED'] = login_disabled
