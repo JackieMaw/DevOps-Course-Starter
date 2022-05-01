cd9d9b6c95152362962a9388a2bc02603186cbfc
JackieMaw <jackiemaw@gmail.com>
2022-05-01 16:00:09 +0100

    configured app service

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "azurerm_cosmosdb_account" "main" {
 }
 
 resource "azurerm_cosmosdb_mongo_database" "main" {
-  name                = "jackiemaw-cosmos-mongo-db"
+  name                = "doMeNowDatabase"
   resource_group_name = azurerm_cosmosdb_account.main.resource_group_name
   account_name        = azurerm_cosmosdb_account.main.name
 }
@@ -86,10 +86,18 @@ resource "azurerm_app_service" "main" {
   app_service_plan_id = azurerm_app_service_plan.main.id
   site_config {
     app_command_line = ""
-    linux_fx_version = "DOCKER|appsvcsample/python-helloworld:latest"
+    linux_fx_version = "DOCKER|jackiemaw/do-me:latest"
   }
   app_settings = {
     "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+	"FLASK_APP"="todo_app/app."
+	"SECRET_KEY" = "AzureSecretKey"
 	"MONGODB_CONNECTION_STRING" = azurerm_cosmosdb_account.main.connection_strings[0]
+	"MONGODB_DATABASE" = "doMeNowDatabase"
+	"CLIENT_ID" = "2b0847670df12f233229"
+	"CLIENT_SECRET" = "84d92032ade58c6dd882655e038900abb3ff0163"
   }
 }
+
+
+ 
