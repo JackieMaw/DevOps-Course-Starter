1749af37d0c7f07b92bc20ac852eaaf02c4889a0
JackieMaw <jackiemaw@gmail.com>
2022-06-24 16:13:04 +0100

    added default port for gunicorn

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Launching application from Gunicorn..."
-poetry run gunicorn "todo_app.app:create_app()" --bind 0.0.0.0:$PORT
+poetry run gunicorn "todo_app.app:create_app()" --bind 0.0.0.0:${PORT:-5000}
 echo "Application launched. (hopefully)"
